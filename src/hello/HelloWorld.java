31b71df055ad254d06a95877b277538348ec802b
Éamonn Reilly <eamonnreilly87@hotmail.com>
2021-07-11 14:03:51 +0100

    formatting

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -11,61 +11,60 @@ public class HelloWorld {
 		int result = solution(S1);
 		System.out.println(S1);
 		System.out.println(result);
-		
+
 		result = solution(S2);
 		System.out.println(S2);
 		System.out.println(result);
 	}
-	
+
 	public static int solution(String S) {
 		int result = 0;
 		int longestCall = 0;
 		Map<String, Integer> listOfPhoneNumbers = new HashMap<String, Integer>();
-		
+
 		String[] data = S.split("\n");
-		
-		for (int i=0; i < data.length; i++) {
+
+		for (int i = 0; i < data.length; i++) {
 			String[] line = data[i].split(",");
-			
+
 			int callDuration = 0;
-			
+
 			String phoneNumber = line[1];
 			if (listOfPhoneNumbers.containsKey(phoneNumber)) {
 				callDuration += listOfPhoneNumbers.get(phoneNumber);
 			}
-			
-			String[] timeOfCall = line[0].split(":");			
+
+			String[] timeOfCall = line[0].split(":");
 			callDuration += getCallDurationSeconds(timeOfCall);
-			
+
 			listOfPhoneNumbers.put(phoneNumber, callDuration);
 		}
-		
-		for(String key: listOfPhoneNumbers.keySet()) {
+
+		for (String key : listOfPhoneNumbers.keySet()) {
 			int price = 0;
-			
+
 			int callDuration = listOfPhoneNumbers.get(key);
 			price = getPrice(callDuration);
-			
-			if (callDuration > longestCall)
-			{
+
+			if (callDuration > longestCall) {
 				longestCall = callDuration;
 			}
-			
+
 			result += price;
 		}
-		
+
 		result -= getPrice(longestCall);
-		
+
 		return result;
 	}
-	
+
 	public static int getPrice(int callDuration) {
 		int price = 0;
 		Double acurateDouble = Double.valueOf(callDuration);
 		if (acurateDouble < 300) {
 			price = (int) (acurateDouble * 3);
-		}else {
-			price = (int) (Math.ceil(acurateDouble/60) * 150);				
+		} else {
+			price = (int) (Math.ceil(acurateDouble / 60) * 150);
 		}
 
 		return price;
@@ -73,9 +72,9 @@ public class HelloWorld {
 
 	public static int getCallDurationSeconds(String[] timeOfCall) {
 		int callDuration = 0;
-		
-		callDuration += Integer.parseInt(timeOfCall[0])*60*60;
-		callDuration += Integer.parseInt(timeOfCall[1])*60;
+
+		callDuration += Integer.parseInt(timeOfCall[0]) * 60 * 60;
+		callDuration += Integer.parseInt(timeOfCall[1]) * 60;
 		callDuration += Integer.parseInt(timeOfCall[2]);
 
 		return callDuration;
